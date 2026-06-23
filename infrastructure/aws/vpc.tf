9b25567fca31b7369971551c2ad2282c76c8cefe
kenneth65936 <www.nathanfla@gmail.com>
2026-06-23 10:51:38 +0200

    fix: use var.public_subnets/var.private_subnets count for route table associations

--- infrastructure/aws/vpc.tf
+++ infrastructure/aws/vpc.tf
@@ -138,7 +138,7 @@ resource "aws_route_table" "public" {
 # ============================================
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnets)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -170,7 +170,7 @@ resource "aws_route_table" "private" {
 # ============================================
 
 resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
+  count          = length(var.private_subnets)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[var.environment == "production" ? count.index : 0].id
 }
